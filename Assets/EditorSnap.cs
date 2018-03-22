741f945db124a8e8edc0e524d608c928be4389ac
Xantavius <git@m-g-p.nl>
2018-03-22 13:25:48 +0100

    RR 01

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour {
+
+
+    [Range(1f,20f)] [SerializeField] float gridSide = 10f;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSide) * gridSide;
+        snapPos.y = 0f;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSide) * gridSide; ;
+
+        transform.position = new Vector3(snapPos.x, snapPos.y, snapPos.z);
+	}
+}
