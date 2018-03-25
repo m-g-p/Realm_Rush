205a459639f2855ec56361c6bc889d631b1dc554
Xantavius <git@m-g-p.nl>
2018-03-25 10:08:04 +0200

    RR 17

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+
+    [SerializeField] int playerHealth = 10;
+    [SerializeField] int enemyHitDamage = 2;
+
+
+    private void Update()
+    {
+        CheckHealth();
+    }
+
+    private void CheckHealth()
+    {
+        if(playerHealth <= 0)
+        {
+            print("You Died!!!");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        BaseDamaged();
+    }
+
+    private void BaseDamaged()
+    {
+            playerHealth = playerHealth - enemyHitDamage;   
+    }
+}
